a803cbbc4ea204b17f18e5244ca0cedb524b310d
Marta Dylewska <mdylewska@bravurasolutions.com>
2023-08-08 14:17:11 +0200

    Initial files

---
+++ ChatAndSocket/src/main/java/com/example/chatandsocket/ChatAndSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatandsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAndSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAndSocketApplication.class, args);
+	}
+
+}
---
+++ ChatAndSocket/src/test/java/com/example/chatandsocket/ChatAndSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatandsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAndSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
