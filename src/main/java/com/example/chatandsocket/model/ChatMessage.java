de35af0595187122fe98555837500a02f1e5912d
Marta Dylewska <mdylewska@bravurasolutions.com>
2023-08-08 16:00:33 +0200

    ChatMessage added

---
+++ src/main/java/com/example/chatandsocket/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.example.chatandsocket.model;
+
+public class ChatMessage {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ChatMessage(String value) {
+        this.value = value;
+    }
+}
